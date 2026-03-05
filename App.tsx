4fe0e0698fa60dac57748770bae5eb5d0b0653ac
Tobias <samueltobiasrolanduyet@gmail.com>
2026-03-05 17:43:12 +0100

    add initial project configuration and implement delete functionality in App component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "It will be gone for good.",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok Deleting"),
+          style: "destructive",
+        },
+        { text: "Cancel", onPress: () => console.log("no"), style: "cancel" },
+      ],
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDelete}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+
+  button: {
+    backgroundColor: theme.colorBlack,
+    color: theme.colorWhite,
+    padding: 8,
+    borderRadius: 6,
+  },
+
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
---
+++ theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colorCerulean: "#1a79f",
+  colorWhite: "#fff",
+  colorBlack: "#000",
+};
